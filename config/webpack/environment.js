661ee2d5159eb1508b1860221ba4e41ab50409b3
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-06-10 05:37:55 +0000

    [update]6/10

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,3 @@
 const { environment } = require('@rails/webpacker')
-//以下追加した部分
-//const { ManifestPlugin } = require('webpack');
 
-//environment.plugins.append(
-  //'ManifestPlugin',
-  //new ManifestPlugin()
-//);
-//ここまで
-
-module.exports = environment;
+module.exports = environment
